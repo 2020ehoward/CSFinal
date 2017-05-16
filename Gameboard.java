a7d42bba9cce3824178c9f39c40e92557fdfe7b8
Evan Howard <2020ehoward@tjhsst.edu>
2017-05-16 14:14:03 -0400

    Made the textures resize to the buttons

--- Gameboard.java
+++ Gameboard.java
@@ -4,30 +4,39 @@ import java.awt.*;
 
  public class Gameboard extends JPanel
 {
-    private final int WIDTH = 15;
+    private final int WIDTH = 12;
     private int[][] map = {
-            {1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,0,0,0,0,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,0,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,0,0,0,0,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1},
-            {0,0,0,0,1,1,1,1,0,1,1,1,1,1,1},
-            {1,1,1,0,1,1,1,1,0,1,1,1,1,1,1},
-            {1,1,1,0,0,0,0,0,0,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,0,1,1,1,1,1,1,1,1,1,1,},
+            {1,1,0,0,0,0,1,1,1,1,1,1,1,},
+            {1,1,1,1,1,0,1,1,1,1,1,1,1,},
+            {1,1,1,1,1,0,0,0,0,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,0,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,0,1,1,1,1,},
+            {0,0,0,0,1,1,1,1,0,1,1,1,1,},
+            {1,1,1,0,1,1,1,1,0,1,1,1,1,},
+            {1,1,1,0,0,0,0,0,0,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,}
     };
     private JButton[][] board;
    public Gameboard()
    {
         board = new JButton[WIDTH][WIDTH];
+
+
        ImageIcon grass = new ImageIcon("Textures/Grass.jpg");
        ImageIcon dirt = new ImageIcon("Textures/Dirt.jpg");
+
+       Image iGrass = grass.getImage();
+       Image resizedGrass = iGrass.getScaledInstance(800/WIDTH,800/WIDTH,java.awt.Image.SCALE_SMOOTH);
+       grass = new ImageIcon(resizedGrass);
+
+       Image iDirt = dirt.getImage();
+       Image resizedDirt = iDirt.getScaledInstance(800/WIDTH,800/WIDTH,java.awt.Image.SCALE_SMOOTH);
+       dirt = new ImageIcon(resizedDirt);
+
        setLayout(new GridLayout(WIDTH,WIDTH));
        for(int x=0;x<WIDTH;x++)
            for(int y=0;y<WIDTH;y++) {
