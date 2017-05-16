1b0eb9c89006bc04614a9d594f0ef33b3c7bdd3a
Evan Howard <2020ehoward@tjhsst.edu>
2017-05-16 13:47:07 -0400

    Added basic 2d button array plus textures

--- Gameboard.java
+++ Gameboard.java
@@ -1,11 +1,42 @@
 //Evan Howard, 11 May 2017
 import javax.swing.*;
-import java.util.*;
+import java.awt.*;
 
-public class Gameboard extends JPanel
-{   
+ public class Gameboard extends JPanel
+{
+    private int[][] map = {
+            {1,1,0,1,1,1,1,1,1,1},
+            {1,1,0,0,0,0,1,1,1,1},
+            {1,1,1,1,1,0,1,1,1,1},
+            {1,1,1,1,1,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,0,1},
+            {1,1,1,1,1,1,1,1,0,1},
+            {0,0,0,0,1,1,1,1,0,1},
+            {1,1,1,0,1,1,1,1,0,1},
+            {1,1,1,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1,1},
+    };
+    private JButton[][] board;
    public Gameboard()
    {
+        board = new JButton[10][10];
+       ImageIcon grass = new ImageIcon("Textures/Grass.jpg");
+       ImageIcon dirt = new ImageIcon("Textures/Dirt.jpg");
+       setLayout(new GridLayout(10,10));
+       for(int x=0;x<10;x++)
+           for(int y=0;y<10;y++) {
+               board[x][y] = new JButton();
+               if(map[x][y]==1) {
+                   board[x][y].setIcon(grass);
+                   board[x][y].setDisabledIcon(grass);
+               }
+                   else {
+                   board[x][y].setIcon(dirt);
+                   board[x][y].setDisabledIcon(dirt);
+               }
+               board[x][y].setEnabled(false);
+               add(board[x][y]);
+       }
    }
    
 }
